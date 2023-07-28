e18e59ed4192e5cc925f36b6e4ab6aaa159cd2a0
Alessio Michelini <amichelini@hubspot.com>
2023-07-28 14:52:30 +0100

    [bugfix] Fix issue with chat code [bugfix] Remove theme

--- static/modules/event.js
+++ static/modules/event.js
@@ -4,12 +4,18 @@ const CHAT_DIRECTION = 'UP';
 
 const createChatMessage = (payload, type = 'question') => {
   const msg = document.createElement('li');
-  msg.classList.add('chat-message', type);
+  msg.classList.add('chat-message');
+
   const span = document.createElement('span');
+  const spanClass = type === 'question' ? 'username' : 'genius';
+  span.classList.add(spanClass)
   span.textContent = `From: ${payload.userName}`;
+
   msg.appendChild(span);
   const paragraph = document.createElement('p');
+  paragraph.classList.add(type);
   paragraph.textContent = payload.message;
+
   msg.appendChild(paragraph);
   const injectMethod = CHAT_DIRECTION === 'UP' ? 'prepend' : 'append';
   chatLog[injectMethod](msg);
