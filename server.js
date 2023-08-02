bb0f64985da2e31c4e301576c1584fc750b228aa
Alessio Michelini <amichelini@hubspot.com>
2023-08-02 15:06:57 +0100

    [bugfix] Code cleanups

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
+const path = require('path');
 const express = require('express');
-const app = express();
 const http = require('http');
 const cors = require('cors');
-const server = http.createServer(app);
 const { Server } = require('socket.io');
+
+require('dotenv').config();
+
+const app = express();
+const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: '*'
   }
 });
+
 const { dispatcher } = require('./lib/actions');
 const port = process.env.PORT || 3000
-const path = require('path');
-
-require('dotenv').config();
 
 app.use(cors());
 app.use(
